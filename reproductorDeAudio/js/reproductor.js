bb4d90dcbea8e36e23b77a29d0504caa014951b1
MontserratLili <lauraliligm@gmail.com>
2017-03-27 15:10:34 -0600

    creando reproductor de audio

---
+++ reproductorDeAudio/js/reproductor.js
@@ -0,0 +1,19 @@
+function play(){
+  document.getElementById("play") = lucy.play;
+
+}
+function stop(){
+  alert("stop");
+}
+function pause(){
+  alert("pause");
+}
+function advance(){
+  alert("advance");
+}
+function back(){
+  alert("back");
+}
+function volume(){
+  alert("volume");
+}
